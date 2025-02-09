d5e11ecf9e0929c88fd78c5fd0b65ee904a5941b
Carlos Moran <karyngadm@gmail.com>
2025-02-08 20:23:24 -0500

    inicializacion de proyecto express

---
+++ src/server.js
@@ -0,0 +1,21 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Ruta de prueba
+app.get('/', (req, res) => {
+    res.send(`API funcionando en el puerto ${process.env.PORT} 🚀`);
+});
+
+// Puerto del servidor
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
+
